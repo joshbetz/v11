b69303ce130c68fb0994aa050b1cc55ff0e2cd20
Josh Betz <j@joshbetz.com>
2012-08-10 00:44:11 -0500

    Link post formats should still link to the source, even if it's a single post

--- loop.php
+++ loop.php
@@ -22,19 +22,14 @@
   		<h1><?php the_title(); ?></h1>
   	</header>
 
-  <?php elseif ( is_single() ): ?>
-
-  	<header>
-  		<h1><?php the_title(); ?></h1>
-  		<span class="date"><?php the_time( get_option( 'date_format' ) ); ?></span>
-  	</header>
-
   <?php else: ?>
     
       <header>
         <?php if ( get_post_format() == "link" ):
           $link = get_post_meta($post->ID, '_format_link_url', true); ?>
           <h1><a href="<?php echo $link; ?>"><?php the_title(); ?></a></h1>
+        <?php elseif ( is_single() ): ?>
+          <h1><?php the_title(); ?></h1>
         <?php else: ?>
           <h1><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
         <?php endif; ?>
