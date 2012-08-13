0d2c6e7a2a714006fb9b0f57af65842ffba86375
Josh Betz <j@joshbetz.com>
2012-08-13 15:22:37 -0500

    Add attribute link in the footer.

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
       </div>
     <footer id="footer">
       <span class="footer-left">&copy; 2012 Josh Betz</span>
-      <span class="footer-right">Proudly powered by WordPress</span>
+      <span class="footer-right">v11 Theme by <a href="http://joshbetz.com">Josh Betz</a> &middot; Proudly powered by WordPress</span>
     </footer>
   </div>
   <!-- JavaScript at the bottom for fast page loading -->
