b5c39e4e8d82f1d1059fbefe47fa71ff90d2892c
Josh Betz <j@joshbetz.com>
2012-08-15 21:40:59 -0500

    Stole arrows from @chexee

--- index.php
+++ index.php
@@ -11,8 +11,8 @@
 	<?php if ( $wp_query->max_num_pages > 1 ) : ?>
 		<nav class="navigation" role="navigation">
 			<h3 class="assistive-text"><?php _e( 'Post navigation', 'twentytwelve' ); ?></h3>
-			<div class="nav-previous alignleft"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', 'twentytwelve' ) ); ?></div>
-			<div class="nav-next alignright"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', 'twentytwelve' ) ); ?></div>
+			<div class="nav-previous alignleft"><?php next_posts_link( __( '<span class="meta-nav">❮</span> Older posts', 'twentytwelve' ) ); ?></div>
+			<div class="nav-next alignright"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">❯</span>', 'twentytwelve' ) ); ?></div>
 		</nav><!-- #nav-above -->
 	<?php endif; ?>
 
