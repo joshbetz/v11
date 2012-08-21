3cc28f38dcaadb0aeec3fd3562e97118edbb8a7b
Josh Betz <j@joshbetz.com>
2012-08-20 21:45:49 -0500

    i18n 404 header

--- 404.php
+++ 404.php
@@ -4,7 +4,7 @@
 
 	<article <?php post_class(); ?>>
 		<header>
-			<h1>404: Four oh Four.</h1>
+			<h1><?php _e( '404: Four oh Four.', 'v11' ); ?></h1>
 		</header>
 		<div class="article">
 			<p><?php _e( "It seems you've reached a page that doesn't exist. Try using the search form below.", 'v11' ); ?></p>
