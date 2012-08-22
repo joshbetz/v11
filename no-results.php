0643f82b6b1311850394a6e2d724628a0a4425fa
Josh Betz <j@joshbetz.com>
2012-08-21 22:13:34 -0500

    Change textdomain to v11

--- no-results.php
+++ no-results.php
@@ -6,16 +6,16 @@
 	<div class="entry-content">
 		<?php if ( is_home() ) { ?>
 
-			<p><?php printf( __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', '_s' ), admin_url( 'post-new.php' ) ); ?></p>
+			<p><?php printf( __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'v11' ), admin_url( 'post-new.php' ) ); ?></p>
 
 		<?php } elseif ( is_search() ) { ?>
 
-			<p><?php _e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', '_s' ); ?></p>
+			<p><?php _e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'v11' ); ?></p>
 			<?php get_search_form(); ?>
 
 		<?php } else { ?>
 
-			<p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', '_s' ); ?></p>
+			<p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'v11' ); ?></p>
 			<?php get_search_form(); ?>
 
 		<?php } ?>
