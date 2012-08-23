2af511c10a805101f2b26fbc62e0d55ee3603ceb
Josh Betz <j@joshbetz.com>
2012-08-22 22:18:46 -0500

    Make Wisconsin big!

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 			</div>
 		<footer id="footer">
 			<span class="footer-left"><?php echo str_replace( "{yyyy}", date('Y'), get_theme_mod( 'v11_copyright' ) ); ?></span>
-			<span class="footer-right"><?php _e( 'v11 Theme by <a href="http://joshbetz.com">Josh Betz</a> in <span aria-hidden="true" data-icon="&#x76;"></span><span class="assistive-text">Wisconsin</span>', 'v11' ); ?> &middot; <?php _e( 'Proudly powered by WordPress', 'v11' ); ?></span>
+			<span class="footer-right"><?php _e( 'v11 Theme made by <a href="http://joshbetz.com">Josh Betz</a> in <span class="wisconsin" aria-hidden="true" data-icon="&#x76;"></span><span class="assistive-text">Wisconsin</span>.', 'v11' ); ?> <?php _e( 'Proudly powered by WordPress', 'v11' ); ?></span>
 		</footer>
 	</div>
 	<!-- JavaScript at the bottom for fast page loading -->
