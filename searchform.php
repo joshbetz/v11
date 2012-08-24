ff16d9651b5ca291f18e86b29396bf4a61251db8
Josh Betz <j@joshbetz.com>
2012-08-24 12:07:21 -0500

    Use get_search_query instead of grabbing the request variable directly

--- searchform.php
+++ searchform.php
@@ -1,4 +1,4 @@
 <form role="search" action="<?php echo home_url(); ?>">
 	<label class="assistive-text" for="s">Search for:</label>
-	<input id="searchbox" name="s" type=type placeholder="Search..." value="<?php if ( isset( $_REQUEST['s'] ) ) echo esc_attr( $_REQUEST['s'] ); ?>">
+	<input id="searchbox" name="s" type=type placeholder="Search..." value="<?php echo get_search_query(); ?>">
 </form>
