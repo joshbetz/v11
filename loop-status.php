cd309b33bc324b15c28859da9a87573d8f9a431c
Josh Betz <j@joshbetz.com>
2012-09-03 15:10:48 -0500

    Fix indentation in status loop

--- loop-status.php
+++ loop-status.php
@@ -1,13 +1,13 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class( 'status' ); ?>>
 
-<?php
-	echo '<div class="article">';
-	the_content();
-	echo '</div>';
-?>
+	<?php
+		echo '<div class="article">';
+		the_content();
+		echo '</div>';
+	?>
 
-<header>
-	<?php get_template_part( 'content-meta' ); ?>
-</header>
+	<header>
+		<?php get_template_part( 'content-meta' ); ?>
+	</header>
 
 </article>
