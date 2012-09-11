d67d529d02374b941791cd09d9d14dbba9a2e87f
Josh Betz <j@joshbetz.com>
2012-09-10 23:03:17 -0500

    Add comment count links

--- content-meta.php
+++ content-meta.php
@@ -1,3 +1,12 @@
+<?php
+	$comment_count = get_comments_number();
+	$link = get_comments_link();
+
+	$comment_count_title = $comment_count == 1 ? 'One' : $comment_count;
+
+	if ( $comment_count )
+		printf( '<div class="comment-count"><a href="%s" title="%s">%s</a></div>', get_comments_link(), $comment_count_title . ' Comments', $comment_count );
+?>
 <div class="post-meta">
 	<span class="meta-date"><?php echo ( time() - get_the_time( 'U' ) < 86400 ? human_time_diff( get_the_time( 'U' ), current_time( 'timestamp' ) ) . ' ago' : get_the_time( get_option( 'date_format' ) ) ); ?></span>
 	<span class="meta-shortlink"><span class="icon" aria-hidden="true" data-icon="&#x22;"></span><span class="assistive-text">Shortlink: </span><?php echo the_shortlink(); ?></span>
