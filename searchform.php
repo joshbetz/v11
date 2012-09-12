04e694bf32001f18db64e7f5ba44a6cbc38075b0
Josh Betz <j@joshbetz.com>
2012-09-11 19:29:24 -0500

    The for attribute is an element ID, not name.

--- searchform.php
+++ searchform.php
@@ -1,4 +1,4 @@
 <form role="search" action="<?php echo home_url(); ?>">
-	<label class="assistive-text" for="s">Search for:</label>
+	<label class="assistive-text" for="searchbox">Search for:</label>
 	<input id="searchbox" name="s" type=text placeholder="Search..." value="<?php the_search_query(); ?>">
 </form>
