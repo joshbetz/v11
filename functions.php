323a67f0c378b45acf05dc667cc86e06a72e94d8
Josh Betz <j@joshbetz.com>
2012-09-15 12:15:24 -0500

    Apply menu_item_has_children for arrays

--- functions.php
+++ functions.php
@@ -184,7 +184,7 @@ class V11_Theme {
 	}
 
 	function menu_item_has_children( $classes, $item, $args ) {
-		if ( isset( $args->has_children ) && $args->has_children )
+		if ( ( isset( $args->has_children ) && $args->has_children ) || ( isset( $args['has_children'] ) && $args['has_children'] ) )
 			$classes[] = 'menu-item-has-children';
 		return $classes;
 	}
