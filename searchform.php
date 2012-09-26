c4102b381616110397084ec6ed42dd16a87c5344
Josh Betz <j@joshbetz.com>
2012-09-26 12:54:02 -0500

    Add search button to search form

--- searchform.php
+++ searchform.php
@@ -1,4 +1,5 @@
 <form role="search" action="<?php echo home_url(); ?>">
 	<label class="assistive-text" for="searchbox">Search for:</label>
 	<input id="searchbox" name="s" type=text placeholder="Search..." value="<?php the_search_query(); ?>">
+	<input class="button" type=submit value="Search">
 </form>
